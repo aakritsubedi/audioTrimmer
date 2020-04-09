877a45668c599a8442d3eeeab22d6ce5488c1a42
Aakrit Subedi <aakritsubedi9@gmail.com>
2020-04-09 09:35:03 +0545

    AudioTrimmer: Made audioTrimmer function more generic

--- audio_trimmer.py
+++ audio_trimmer.py
@@ -2,16 +2,15 @@ from pydub import AudioSegment
 import sys
 sys.path.append('/usr/bin/ffmpeg')
 
-def audioTrimmer(file_name,startMin,startSec,endMin,endSec):
-  files_path = 'mp3/'
+def audioTrimmer(files_path,file_name,fileType,startMin,startSec,endMin,endSec):
 
   # Time to miliseconds
   startTime = startMin*60*1000+startSec*1000
   endTime = endMin*60*1000+endSec*1000
 
   # Opening file and extracting segment
-  song = AudioSegment.from_mp3( files_path+file_name+'.mp3' )
+  song = AudioSegment.from_file( files_path+'/'+file_name, fileType )
   extract = song[startTime:endTime]
 
   # Saving
-  extract.export( files_path+file_name+'-extract.mp3', format="mp3")
+  extract.export( files_path+'/'+file_name+'-extract.mp3', format=fileType)
