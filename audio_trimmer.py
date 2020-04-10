1e2e53ef911ba8ae0e4b6c483a68d4f41cffba78
Aakrit Subedi <aakritsubedi9@gmail.com>
2020-04-10 14:29:12 +0545

    AudioTrimmer: Update functionality to export

--- audio_trimmer.py
+++ audio_trimmer.py
@@ -3,14 +3,16 @@ import sys
 sys.path.append('/usr/bin/ffmpeg')
 
 def audioTrimmer(files_path,file_name,fileType,startMin,startSec,endMin,endSec):
+  MIN = 60
+  MILI_SECOND = 1000
 
   # Time to miliseconds
-  startTime = startMin*60*1000+startSec*1000
-  endTime = endMin*60*1000+endSec*1000
+  startTime = startMin*MIN*MILI_SECOND+startSec*MILI_SECOND
+  endTime = endMin*MIN*MILI_SECOND+endSec*MILI_SECOND
 
   # Opening file and extracting segment
-  song = AudioSegment.from_file( files_path+'/'+file_name, fileType )
-  extract = song[startTime:endTime]
+  segment = AudioSegment.from_file( files_path+'/'+file_name, fileType )
+  extract = segment[startTime:endTime]
 
   # Saving
-  extract.export( files_path+'/'+file_name+'-extract.mp3', format=fileType)
+  extract.export( files_path+'/'+file_name+'-extract.'+fileType, format=fileType)
